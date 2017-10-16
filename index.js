5f1745e0be003dcd78c26138c025554a9038aeb5
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-10-16 06:48:42 +0100

    Minor ammend.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const corsMiddleware = require('restify-cors-middleware')
 
 const cors = corsMiddleware({
  preflightMaxAge: 5, //Optional
- origins: ['https://parking-app-admin.herokuapp.com', 'http://localhost:4200', 'http://myparking-app-admin.azurewebsites.net', 'http://localhost:8100/'],
+ origins: ['https://parking-app-admin.herokuapp.com', 'http://localhost:4200', 'http://myparking-app-admin.azurewebsites.net', 'http://localhost:8100'],
  allowHeaders: ['API-Token'],
  exposeHeaders: ['API-Token-Expiry']
 })
